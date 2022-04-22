a42cbe8c680d453a891f36044a058eb22caf47a5
Ubuntu <ubuntu@jrp-acdc-smilecdr-20220202.novalocal>
2022-04-22 03:02:38 +0000

    update nginx

--- dump_db.sh
+++ dump_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/smilecdr_repo
-docker exec -it postgres_cdr /usr/bin/pg_dump --username=postgres cdr > ./sql/cdr.sql
-docker exec -it postgres_cdr /usr/bin/pg_dump --username=postgres cdr_sys > ./sql/cdr_sys.sql
+docker exec -it postgres_cdr /usr/bin/pg_dump --username=postgres cdr > /home/ubuntu/smilecdr_repo/sql/cdr.sql
+docker exec -it postgres_cdr /usr/bin/pg_dump --username=postgres cdr_sys > /home/ubuntu/smilecdr_repo/sql/cdr_sys.sql
